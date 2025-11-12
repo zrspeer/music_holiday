ef8564a53b0b93b9d168659913c55445d85a0f34
zrspeer <zrspeer@icloud.com>
2025-11-12 14:31:39 -0500

    Forms added. Now a bare site that writes to db with user input

--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -20,3 +21,35 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'questions/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('questions:topics')
+    # Display blank or invalid form.
+    context = {'form': form}
+    return render(request, 'questions/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Add a new entry for a particular topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('questions:topic', topic_id=topic_id)
+    # Display a blank or invalid form.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'questions/new_entry.html', context)
