2ff1c5a4c72de7eacdde652fe811cc009f898c07
zrspeer <zrspeer@icloud.com>
2025-11-18 13:31:20 -0500

    updated admin portal for better answer viewing

--- questions/admin.py
+++ questions/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import Answer, Question, Submission
 
 
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 0
+    fields = ("question", "value_text", "short_value")
+    readonly_fields = ("short_value",)
+
+    def short_value(self, obj):
+        return (obj.value_text or "")[:60]
+
+
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ("prompt", "order", "slug")
@@ -13,12 +23,13 @@ class QuestionAdmin(admin.ModelAdmin):
 class SubmissionAdmin(admin.ModelAdmin):
     list_display = ("name", "created")
     search_fields = ("name",)
+    inlines = [AnswerInline]
 
 
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
-    list_display = ("question", "value_text", "short_value")
-    search_fields = ("value_text", "question_prompt", "submission_nickname")
+    list_display = ("submission", "question", "short_value")
+    search_fields = ("value_text", "question__prompt", "submission__name")
     autocomplete_fields = ("submission", "question")
 
     def short_value(self, obj):
