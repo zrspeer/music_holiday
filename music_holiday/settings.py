4b889c0d8ebd4485f92181a919782c1985475a70
zrspeer <zrspeer@icloud.com>
2025-11-18 19:21:01 -0500

    settings changes for STATIC_ROOT

--- music_holiday/settings.py
+++ music_holiday/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
